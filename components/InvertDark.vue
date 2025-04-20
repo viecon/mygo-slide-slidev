863d757ca1cdff7e34fa2f999cf4ae0b9d2df715
viecon <twshawn28e7@gmail.com>
2025-04-20 18:08:30 +0800

    Refactor InvertDark component styles and clean up global-bottom template formatting

--- components/InvertDark.vue
+++ components/InvertDark.vue
@@ -5,11 +5,6 @@
 </template>
 
 <style scoped>
-.invert-dark {
-  /* Only invert in dark mode */
-  /* Slidev applies the class 'dark' to the <html> tag */
-}
-
 .dark .invert-dark {
   filter: invert(1) hue-rotate(180deg);
 }
